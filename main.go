4aac874b69ae022dbeb9fe70072d58ecf3474835
The Greatest Giant <seanalter1@gmail.com>
2023-09-26 11:32:11 -0400

    Tests
    tests for searching a directory

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
+func main() {
+	home := os.Getenv("HOME")
+	// files, err := os.ReadDir(home)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	err := filepath.Walk(home+"/.config",
+		func(path string, _ os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			fmt.Println(path)
+			return nil
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
